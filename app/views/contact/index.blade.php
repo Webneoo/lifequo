a754934863ca8320394a5b0119fa4e97b208b99c
Webneoo <info@webneoo.com>
2015-10-30 20:18:23 +0200

    nour_adding_catalog

--- app/views/contact/index.blade.php
+++ app/views/contact/index.blade.php
@@ -88,8 +88,7 @@
 	</div>
 
 	<div class="col-md-6">
-		<iframe class="img-responsive hidden-xs hidden-sm" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d828.0280063549536!2d35.51459240118485!3d33.88676859209987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151f16ffd3b5c739%3A0x9e545ad0e6f4a476!2sAchrafieh!5e0!3m2!1sen!2slb!4v1446225250908" width="540" height="412" frameborder="0" style="border:0" allowfullscreen></iframe>
-		
+		<img class="img-responsive hidden-xs hidden-sm" src="../images/map.png" alt="MAP">
 		<div class="row">
 			<h2>When can we visit you ?</h2>
             <div class="form-group contact-form">
