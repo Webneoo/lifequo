390a3e36a6f2c781a7304b4f248ebf0875d3d65b
Nour Fakhoury <info@webneoo.com>
2016-08-01 15:27:26 +0300

    resizing the expertise images

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -202,67 +202,65 @@
 		<span class="line hidden-xs" style="margin:-63px auto auto 120px; background-color:#fff"></span>
 			
 			<div class="row">
-			<div class="col-sm-3">
-                <div class="team-member">
-                    <img src="/images/team/farid.jpg" class="img-responsive img-circle" alt="Farid Abi Aad | Mechanical engineer with 30 years of experience" title="Farid Abi Aad | Mechanical engineer with 30 years of experience">
-                    <h3>Farid Abi Aad</h3>
-                    <h4 class="text-muted">Mechanical engineer with 30 years of experience</h4>
-                </div>
-            </div>
+				<div class="col-sm-3">
+	                <div class="team-member">
+	                    <img src="/images/team/tony.jpg" class="img-responsive img-circle" alt="Tony Hojeily | Technical manager" title="Tony Hojeily | Technical manager">
+	                    <h3>Tony Hojeily</h3>
+	                    <h4>Technical manager</h4> 
+	                </div>
+	            </div>
 
-            <div class="col-sm-3">
-                <div class="team-member">
-                    <img src="/images/team/assaad.jpg" class="img-responsive img-circle" alt="Assaad Kfoury | Electrical engineer with 20 years of experience" title="Assaad Kfoury | Electrical engineer with 20 years of experience">
-                    <h3>Assaad Kfoury</h3>
-                    <h4>Electrical engineer with 20 years of experience</h4>
-                </div>
-            </div>
+				<div class="col-sm-3">
+	                <div class="team-member">
+	                    <img src="/images/team/farid.jpg" class="img-responsive img-circle" alt="Farid Abi Aad | Mechanical engineer with 30 years of experience" title="Farid Abi Aad | Mechanical engineer with 30 years of experience">
+	                    <h3>Farid Abi Aad</h3>
+	                    <h4 class="text-muted">Mechanical engineer with 30 years of experience</h4>
+	                </div>
+	            </div>
+
+	            <div class="col-sm-3">
+	                <div class="team-member">
+	                    <img src="/images/team/assaad.jpg" class="img-responsive img-circle" alt="Assaad Kfoury | Electrical engineer with 20 years of experience" title="Assaad Kfoury | Electrical engineer with 20 years of experience">
+	                    <h3>Assaad Kfoury</h3>
+	                    <h4>Electrical engineer with 20 years of experience</h4>
+	                </div>
+	            </div>
 
 
-            <div class="col-sm-3">
-                <div class="team-member">
-                    <img src="/images/team/julien.jpg" class="img-responsive img-circle" alt="Julien Mouchantaf | Civil engineer" title="Julien Mouchantaf | Civil engineer">
-                    <h3>Julien Mouchantaf</h3>
-                    <h4>Civil engineer</h4>  
-                </div>
-            </div>
-
-
-            <div class="col-sm-3">
-                <div class="team-member">
-                    <img src="/images/team/tony.jpg" class="img-responsive img-circle" alt="Tony Hojeily | Technical manager" title="Tony Hojeily | Technical manager">
-                    <h3>Tony Hojeily</h3>
-                    <h4>Technical manager</h4> 
-                </div>
-           </div>
+	            <div class="col-sm-3">
+	                <div class="team-member">
+	                    <img src="/images/team/julien.jpg" class="img-responsive img-circle" alt="Julien Mouchantaf | Civil engineer" title="Julien Mouchantaf | Civil engineer">
+	                    <h3>Julien Mouchantaf</h3>
+	                    <h4>Civil engineer</h4>  
+	                </div>
+	            </div>
            </div>
 
            <div class="row">
-           <div class="col-sm-3">
-                <div class="team-member">
-                    <img src="/images/team/corine.jpg" class="img-responsive img-circle" alt="Corine Chadaideh | Accounting department" title="Corine Chadaideh | Accounting department">
-                    <h3>Corine Chadaideh</h3>
-                    <h4>Accounting department</h4>  
-                </div>
-            </div>
+	           <div class="col-sm-3">
+	                <div class="team-member">
+	                    <img src="/images/team/corine.jpg" class="img-responsive img-circle" alt="Corine Chadaideh | Accounting department" title="Corine Chadaideh | Accounting department">
+	                    <h3>Corine Chadaideh</h3>
+	                    <h4>Accounting department</h4>  
+	                </div>
+	            </div>
 
 
-            <div class="col-sm-3">
-                <div class="team-member">
-                    <img src="/images/team/reem.jpg" class="img-responsive img-circle" alt="Reem Bou Matar | Operations manager collection" title="Reem Bou Matar | Operations manager collection">
-                    <h3>Reem Bou Matar</h3>
-                    <h4>Operations manager collection</h4> 
-                </div>
-           </div>
+	            <div class="col-sm-3">
+	                <div class="team-member">
+	                    <img src="/images/team/reem.jpg" class="img-responsive img-circle" alt="Reem Bou Matar | Operations manager collection" title="Reem Bou Matar | Operations manager collection">
+	                    <h3>Reem Bou Matar</h3>
+	                    <h4>Operations manager collection</h4> 
+	                </div>
+	           </div>
 
-           <div class="col-sm-3">
-                <div class="team-member">
-                    <img src="/images/team/bassam.jpg" class="img-responsive img-circle" alt="Bassam Hammoud | Engineer" title="Bassam Hammoud | Engineer">
-                    <h3>Bassam Hammoud</h3>
-                    <h4>Engineer</h4> 
-                </div>
-           </div>
-
+	           <div class="col-sm-3">
+	                <div class="team-member">
+	                    <img src="/images/team/bassam.jpg" class="img-responsive img-circle" alt="Bassam Hammoud | Engineer" title="Bassam Hammoud | Engineer">
+	                    <h3>Bassam Hammoud</h3>
+	                    <h4>Engineer</h4> 
+	                </div>
+	           </div>
            </div>
 	</div>
 </div>
